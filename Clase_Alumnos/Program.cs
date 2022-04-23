410bb8c036c8fcd581116563d2042d00e6d1b3cc
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-04-23 09:09:43 -0700

    Se agrego nuevo alumno y se modificaron los datos

--- Clase_Alumnos/Program.cs
+++ Clase_Alumnos/Program.cs
@@ -3,20 +3,26 @@
 List<Alumno> lista = new List<Alumno>();
 
 Alumno alumno1 = new Alumno();
-alumno1.Nombres = "Antonio";
-alumno1.ApellidoPaterno = "Rogel";
-alumno1.Matricula = "123";
-alumno1.FechaNacimiento = DateTime.Now;
-
+alumno1.Nombres = "Elzar";
+alumno1.ApellidoPaterno = "Machado";
+alumno1.Matricula = "777";
+alumno1.FechaNacimiento = new DateTime(2001, 06, 03);
 
 Alumno alumno2 = new Alumno();
-alumno2.Nombres = "Juan";
-alumno2.ApellidoPaterno = "Lopez";
-alumno2.Matricula = "456";
-alumno2.FechaNacimiento = new DateTime(1980, 02, 28);
+alumno2.Nombres = "Jennifer";
+alumno2.ApellidoPaterno = "Moreno";
+alumno2.Matricula = "666";
+alumno2.FechaNacimiento = new DateTime(2000, 10, 24);
+
+Alumno alumno3 = new Alumno();
+alumno3.Nombres = "Cesar";
+alumno3.ApellidoPaterno = "Denogei";
+alumno3.Matricula = "123";
+alumno3.FechaNacimiento = new DateTime(2001, 06, 02);
 
 lista.Add(alumno1);
 lista.Add(alumno2);
+lista.Add(alumno3);
 
 foreach (Alumno alumnoEnLista in lista)
 {
